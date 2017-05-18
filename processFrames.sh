74e59e34c65f2728d21c5240f5f533260efb9c2e
murphymatt <murphyma@grinnell.edu>
2017-05-19 01:12:54 +0200

    update script

--- processFrames.sh
+++ processFrames.sh
@@ -1,3 +1,6 @@
 ##!/bin/bash
+mkdir frames
+ffmpeg -i videoHD.MOV -r 24 frames/frame_%02d.jpeg
 mkdir frames_out
-./src/imgpro frames4/frame_01.jpeg frames_out/frame_01.jpeg -projectImage ./frames4/frame_01.jpeg
+./src/imgpro frames/frame_01.jpeg frames_out/frame_01.jpeg -projectImage frames/frame_01.jpeg
+ffmpeg -r 24 -i frames_out/frame_%02d.jpeg -c:v libx264 -vf "fps=24,format=yuv420p" out.mp4
